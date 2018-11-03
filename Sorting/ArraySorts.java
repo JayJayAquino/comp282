46bf70229cd711c2d9d8f317bc2d1066076f4a00
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-11-02 23:43:35 -0700

    initial commit

---
+++ Sorting/ArraySorts.java
@@ -0,0 +1,42 @@
+import java.math.*;
+import java.util.Random;
+
+class pair{
+  public int left, right;
+
+  public pair(int left, int right)
+  {
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class ArraySorts{
+
+  public static void insertionSort(int a[], int n)
+  {
+    //
+  }
+
+  public static void QuickSort1(int a[], int n, int cutoff);
+  {
+    //
+  }
+
+  public static void QuickSort2(int a[], int n, int cutoff)
+  {
+    //
+  }
+  public static void QuickSort3(int a[], int n, int cutoff)
+  {
+    //
+  }
+  public static void QuickSort4(int a[], int n, int cutoff)
+  {
+    //
+  }
+  public static void QuickSort5(int a[], int n, int cutoff)
+  {
+    //
+  }
+}
