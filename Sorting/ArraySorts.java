846ba19c5149606fd976e01af5a7fca0e35ac2bd
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-11-10 12:59:16 -0800

    start AlmostQS1

--- Sorting/ArraySorts.java
+++ Sorting/ArraySorts.java
@@ -40,6 +40,7 @@ class ArraySorts{
         tempItem = a[firstUnknown];
         a[firstUnknown] = a[lastS1];
         a[lastS1] = tempItem;
+        System.out.println("partitioning");
       }
     }
     tempItem = a[lf];
@@ -51,13 +52,13 @@ class ArraySorts{
   public static void QuickSort1(int a[], int n, int cutoff)
   {
     QuickSort1(a,0,n-1, cutoff);
+    insertionSort(a, a.length);
+
   }
 
   private static void QuickSort1(int a[], int lf, int rt, int cutoff)
   {
-    if(rt - lf + 1 <= cutoff){
-      insertionSort(a, a.length);
-    }else{
+    while(rt - lf + 1 <= cutoff){
       Random r = new Random();
       int pivot = a[r.nextInt(a.length)];
       pivot = partition(a,lf,rt, pivot);
@@ -65,8 +66,10 @@ class ArraySorts{
       int rightSize = rt - (pivot + 1);
       if(leftSize < rightSize){
         QuickSort1(a,lf,pivot-1,cutoff);
+        rt = pivot+1;
       }else{
         QuickSort1(a,pivot+1,rt,cutoff);
+        lf = pivot-1;
       }
     }
   }
@@ -87,6 +90,33 @@ class ArraySorts{
   // {
   //   //
   // }
+  //
+  public static void AlmostQS1(int a[], int n, int cutoff)
+  {
+    System.out.println("executing AlmostQS1");
+    AlmostQS1(a,0,n-1, cutoff);
+  }
+
+  private static void AlmostQS1(int a[], int lf, int rt, int cutoff)
+  {
+    while(rt - lf + 1 >= cutoff){
+      Random r = new Random();
+      int pivot = a[lf];
+      System.out.println("starting partition");
+      pivot = partition(a,lf,rt, pivot);
+      int leftSize = (pivot - 1) - lf;
+      int rightSize = rt - (pivot + 1);
+      if(leftSize < rightSize){
+        System.out.println("left smaller than right");
+        AlmostQS1(a,lf,pivot-1,cutoff);
+        rt = pivot+1;
+      }else{
+        System.out.println("right smaller than left");
+        AlmostQS1(a,pivot+1,rt,cutoff);
+        lf = pivot-1;
+      }
+    }
+  }
 
   public static String myName()
   {
@@ -97,14 +127,16 @@ class ArraySorts{
   //AlmostQS2
 
     public static void main(String[] args){
-      int arraySize = 10000;
-      int[] a = new int[arraySize];
-      Random r = new Random();
-      for (int i = 0; i < arraySize; i++) {
-          int rand = r.nextInt(100000);
-          a[i] = rand;
-        }
-      QuickSort1(a,a.length,5);
+      // int arraySize = 10;
+      // int[] a = new int[arraySize];
+      // Random r = new Random();
+      // for (int i = 0; i < arraySize; i++) {
+      //     int rand = r.nextInt(500);
+      //     a[i] = rand;
+      //   }
+      // QuickSort1(a,a.length,5);
+      int[] a = {20, 18, 100, 23, 34, 8, 3, 90, 9, 42, 24};
+      AlmostQS1(a,a.length,1);
       for (int i = 0; i < a.length; i++) {
         System.out.println(a[i]);
       }
