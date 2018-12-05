e802db1bda07cde8c14250318e4fbf83602ca4ec
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-12-04 18:44:39 -0800

    begin documentation

--- Sorting/ArraySorts.java
+++ Sorting/ArraySorts.java
@@ -13,6 +13,12 @@ class pair{
 
 class ArraySorts{
 
+  /**
+   * normal insertion sort, operates in O(n^2) time, operates by
+   * shifting elements until array is sorted
+   * @param a[] [array of integers]
+   * @param n   [size of array]
+   */
   public static void insertionSort(int a[], int n)
   {
     int save;
@@ -29,6 +35,14 @@ class ArraySorts{
     }
   }
 
+  /**
+   * Partition method used by QS1, QS3, and AlmostQS1
+   * @param  a[]   [array of integers]
+   * @param  lf    [left most element]
+   * @param  rt    [right most element]
+   * @param  pivot [position of pivot]
+   * @return       [a partitioned array relative to pivot]
+   */
   private static int bookPartition(int a[], int lf, int rt, int pivot)
   {
     int tempItem;
@@ -66,6 +80,15 @@ class ArraySorts{
     return lastS1;
   }
 
+  /**
+   * Partition method used by QS2, QS4, and AlmostQS2
+   * @param  a[]   [array of integers]
+   * @param  lf    [left most element]
+   * @param  rt    [right most element]
+   * @param  pivot [position of pivot]
+   * @return       [a pair of positions indicating where the two partitioned
+   *               arrays start and end]
+   */
   private static pair twoPointPartition(int a[], int lf, int rt, int pivot)
   {
     int leftPointer = lf;
@@ -73,44 +96,37 @@ class ArraySorts{
     int pivotValue = a[pivot];
     int temp;
 
-    // while(leftPointer <= rightPointer){
-    //   if(a[leftPointer] < pivotValue){
-    //     leftPointer++;
-    //   }else{
-    //     if(a[rightPointer] > pivotValue){
-    //       rightPointer--;
-    //     }else{
-    //       //swap
-    //       temp = a[leftPointer];
-    //       a[leftPointer] = a[rightPointer];
-    //       a[rightPointer] = temp;
-    //
-    //       leftPointer++;
-    //       rightPointer--;
-    //     }
-    //   }
-    // }//while
-
     while(leftPointer <= rightPointer){
       while(a[leftPointer] < pivotValue){
         leftPointer++;
-      }
+      }//while
       while(a[rightPointer] > pivotValue){
         rightPointer--;
-      }
+      }//while
       if(leftPointer <= rightPointer){
+        //swap
         temp = a[leftPointer];
         a[leftPointer] = a[rightPointer];
         a[rightPointer] = temp;
 
         leftPointer++;
         rightPointer--;
-      }
-    }
+      }//if
+    }//while
 
     return new pair(leftPointer, rightPointer);
   }
 
+  /**
+   * Partition method used by QS5 and AlmostQS5
+   * @param  a[]        [array of integers]
+   * @param  lf         [left most element]
+   * @param  rt         [right most element]
+   * @param  leftPivot  [position of pivot for less than partition]
+   * @param  rightPivot [position of pivot for greater than partition]
+   * @return            [a pair of positions indicating where the 3
+   *                    partition arrays start and end]
+   */
   private static pair threePartition(int a[], int lf, int rt,
   int leftPivot, int rightPivot)
   {
@@ -120,14 +136,18 @@ class ArraySorts{
     int firstBig = rt;
     boolean duplicate = true;
 
+    //swap
     temp = a[lf];
     a[lf] = a[leftPivot];
     a[leftPivot] = temp;
 
+    //swap
     temp = a[rt];
     a[rt] = a[rightPivot];
     a[rightPivot] = temp;
 
+    //left size contains smaller values,
+    //right side contains bigger values
     if(a[lf] > a[rt]){
       temp = a[lf];
       a[lf] = a[rt];
@@ -136,12 +156,14 @@ class ArraySorts{
 
     while(firstUnknown < firstBig){
       if(a[firstUnknown] < a[lf]){
+        //increment, swap, increment
         lastSmall++;
         temp = a[lastSmall];
         a[lastSmall] = a[firstUnknown];
         a[firstUnknown] = temp;
         firstUnknown++;
       }else if(a[firstUnknown] > a[rt]){
+        //decrement, swap
         firstBig--;
         temp = a[firstBig];
         a[firstBig] = a[firstUnknown];
@@ -178,43 +200,79 @@ class ArraySorts{
     return new pair(lastSmall, firstBig);
   }
 
+  /**
+   * Driver method for Quicksort, uses bookPartition,
+   * will switch to insertionSort once
+   * partitioned arrays are below cutoffValue
+   * @param a[]    [array of integers]
+   * @param n      [size of array]
+   * @param cutoff [partitioned array's max size]
+   */
   public static void QuickSort1(int a[], int n, int cutoff)
   {
     QuickSort1(a,0,n-1, cutoff);
     insertionSort(a, n);
   }
 
+  /**
+   * Uses BookPartition, and selects a random pivot position
+   * Continues until cutoff is reached
+   * @param a[]    [array of integers]
+   * @param lf     [left most element]
+   * @param rt     [right most element]
+   * @param cutoff [partitioned array's max size]
+   */
   private static void QuickSort1(int a[], int lf, int rt, int cutoff)
   {
+    //while loop to avoid stack overflow
     while(rt - lf + 1 >= cutoff){
       Random r = new Random();
-      int pivot = r.nextInt((rt-lf)+1) + lf;
-      pivot = bookPartition(a,lf,rt, pivot);
-      int leftSize = (pivot - 1) - lf;
-      int rightSize = rt - (pivot + 1);
+      int pivot = r.nextInt((rt-lf)+1) + lf;// generate random pivot
+      pivot = bookPartition(a,lf,rt, pivot);// partition array
+      int leftSize = (pivot - 1) - lf;// calculate size of left partition
+      int rightSize = rt - (pivot + 1);// calculate size of right partition
+      //continue to sort the smaller partition, and "pretend" the other
+      //partition is finished
       if(leftSize < rightSize){
         QuickSort1(a,lf,pivot-1,cutoff);
         lf = pivot+1;
       }else{
         QuickSort1(a,pivot+1,rt,cutoff);
         rt = pivot-1;
-      }
-    }
+      }//if
+    }//while
   }
 
+  /**
+   * Driver method for Quicksort, uses 2 pointer partition,
+   * will switch to insertionSort once
+   * partitioned arrays are below cutoffValue
+   * @param a[]    [array of integers]
+   * @param n      [size of array]
+   * @param cutoff [partitioned array's max size]
+   */
   public static void QuickSort2(int a[], int n, int cutoff)
   {
     QuickSort2(a,0,n-1,cutoff);
     insertionSort(a,n);
   }
 
+  /**
+   * Uses 2ptr partition, and selects a random pivot position
+   * Continues until cutoff is reached
+   * @param a[]    [array of integers]
+   * @param lf     [left most element]
+   * @param rt     [right most element]
+   * @param cutoff [partitioned array's max size]
+   */
   private static void QuickSort2(int a[], int lf, int rt, int cutoff)
   {
     pair p;
+    //while loop to avoid stack overflow
     while(rt - lf + 1 >= cutoff){
       Random r = new Random();
       int pivot = r.nextInt((rt-lf)+1) + lf;
-      p = twoPointPartition(a,lf,rt,pivot);
+      p = twoPointPartition(a,lf,rt,pivot); // pair of pivots
       int leftSize = (p.right) - lf;
       int rightSize = rt - (p.left);
       if(leftSize < rightSize){
@@ -228,6 +286,12 @@ class ArraySorts{
 
   }
 
+  /**
+   * Driver for QuickSort3, same logic as QuickSort1
+   * @param a[]    [array of integers]
+   * @param n      [size of array]
+   * @param cutoff [partitioned array's max size]
+   */
   public static void QuickSort3(int a[], int n, int cutoff)
   {
     QuickSort3(a,0,n-1,cutoff);
@@ -235,6 +299,9 @@ class ArraySorts{
   }
 
 
+  /*
+  Exact same logic as QuickSort1, except pivot is left most element
+   */
   private static void QuickSort3(int a[], int lf, int rt, int cutoff)
   {
     while(rt - lf + 1 >= cutoff){
@@ -251,12 +318,17 @@ class ArraySorts{
       }
     }
   }
+
+
   public static void QuickSort4(int a[], int n, int cutoff)
   {
     QuickSort4(a,0,n-1,cutoff);
     insertionSort(a,n);
   }
 
+  /*
+  Exact same logic as QuickSort2, except pivot is the left most element
+   */
   private static void QuickSort4(int a[], int lf, int rt, int cutoff)
   {
     pair p;
@@ -281,6 +353,13 @@ class ArraySorts{
     insertionSort(a,n);
   }
 
+  /**
+   * [QuickSort5 description]
+   * @param a[]    [description]
+   * @param lf     [description]
+   * @param rt     [description]
+   * @param cutoff [description]
+   */
   private static void QuickSort5(int a[], int lf, int rt, int cutoff)
   {
     while(rt - lf + 1 >= cutoff){
@@ -308,23 +387,6 @@ class ArraySorts{
     }
   }
 
-  private static void BuildBU(int a[], int n)
-  {
-    int save;
-    int currentPos;
-    int leftChild;
-    int rightChild;
-    int parent;
-
-    for(int startPos = (n-2)/2; startPos >= 0; startPos++){
-      currentPos = startPos;
-      leftChild = (currentPos*2)+1;
-      rightChild =(currentPos*2)+2;
-      save = a[currentPos];
-      // if(a[leftChild] > a[rightChild])
-    }
-  }
-
   private static void BuildTD(int a[], int n)
   {
     int save;
@@ -344,11 +406,6 @@ class ArraySorts{
     }
   }
 
-  // private static void TrickleUp()
-  // {
-  //
-  // }
-
   private static void TrickleDown(int a[], int top, int bottom)
   {
     int leftChild = (top*2)+1;
@@ -367,12 +424,12 @@ class ArraySorts{
       }
     }
 
-    while((currentPos < bottom) && (greaterChild <= bottom) && (save < a[greaterChild])){
-      // System.out.println("shift " + a[greaterChild]);
-      // System.out.println("current position: " + currentPos);
+    while((currentPos < bottom) &&
+    (greaterChild <= bottom) &&
+    (save < a[greaterChild])){
+
       a[currentPos] = a[greaterChild];
       currentPos = greaterChild;
-      // System.out.println("current pos is now: " + currentPos);
       leftChild = (currentPos * 2) + 1;
       rightChild = (currentPos * 2) + 2;
 
@@ -392,8 +449,6 @@ class ArraySorts{
 
   public static void HeapSortBU(int a[], int n)
   {
-    //build-for loop
-    //sort for loop
     int temp;
     int last = n-1;
     for(int build = last; build >= 0; build--){
@@ -441,6 +496,7 @@ class ArraySorts{
   public static void AlmostQS5(int a[], int n, int cutoff)
   {
     QuickSort5(a,0,n-1,cutoff);
+    // insertionSort(a, a.length);
   }
 
   public static String myName()
